de279ae76fdcc664b9dc5634bda340145cef2787
kreuzerk <kevin.kreuzer90@icloud.com>
2020-03-25 11:29:52 +0100

    feat(svgo): use configuration to config svgo

--- src/bin/svg-to-ts.ts
+++ src/bin/svg-to-ts.ts
@@ -1,21 +1,18 @@
 #!/usr/bin/env node
-
 import { convertToSingleFile } from '../lib/converters/single-file.converter';
 import { convertToMultipleFiles } from '../lib/converters/multiple-files.converter';
 import { getOptions, MultiFileConvertionOptions, SingleFileConvertionOptions } from '../lib/options/convertion-options';
 import { printLogo } from '../lib/helpers/log-helper';
 import { setupCommander } from '../lib/options/args-collector';
 
-const start = async () => {
+(async () => {
   setupCommander();
   printLogo();
   const convertionOptions = await getOptions();
 
   if (convertionOptions.optimizeForLazyLoading) {
-    convertToMultipleFiles(convertionOptions as MultiFileConvertionOptions);
+    await convertToMultipleFiles(convertionOptions as MultiFileConvertionOptions);
   } else {
-    convertToSingleFile(convertionOptions as SingleFileConvertionOptions);
+    await convertToSingleFile(convertionOptions as SingleFileConvertionOptions);
   }
-};
-
-start();
+})();
