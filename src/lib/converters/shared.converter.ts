e611b121a9d01f93184b3eec0c6d732faed95b38
Felipe dos Santos <felipe.santos@fiixsoftware.com>
2020-04-05 11:58:53 -0400

    Add shared file for methods shared by multi and single processors

---
+++ src/lib/converters/shared.converter.ts
@@ -0,0 +1,44 @@
+import { getFilePathsFromRegex } from '../helpers/regex-helpers';
+import * as path from 'path';
+import { extractSvgContent } from '../helpers/file-helpers';
+import { generateTypeName, generateVariableName } from '../generators/code-snippet-generators';
+import { generateSvgOptimizer } from '../helpers/svg-optimization';
+import { info } from '../helpers/log-helper';
+
+export interface SvgDefinition {
+  typeName: string;
+  variableName: string;
+  interfaceName: string;
+  data: string;
+  filenameWithoutEnding: string;
+}
+
+export const filesProcessor = async (convertionOptions): Promise<SvgDefinition[]> => {
+  const { prefix, delimiter, interfaceName, srcFiles, svgoConfig } = convertionOptions;
+  const svgOptimizer = generateSvgOptimizer(svgoConfig);
+  const filePaths = await getFilePathsFromRegex(srcFiles);
+
+  // Using Promise.all we are making all files be processed in parallel as they have no dependency on each other
+  let svgDefinitions = await Promise.all(
+    filePaths.map(
+      async (filePath): Promise<SvgDefinition | null> => {
+        let svgDefinition: SvgDefinition = null;
+        const fileNameWithEnding = path.basename(filePath);
+        const [filenameWithoutEnding, extension] = fileNameWithEnding.split('.');
+        if (extension === 'svg') {
+          const rawSvg = await extractSvgContent(filePath);
+          info(`optimize svg: ${fileNameWithEnding}`);
+          const optimizedSvg = await svgOptimizer.optimize(rawSvg);
+          const variableName = generateVariableName(prefix, filenameWithoutEnding);
+
+          const typeName = generateTypeName(filenameWithoutEnding, delimiter);
+          svgDefinition = { typeName, variableName, interfaceName, data: optimizedSvg.data, filenameWithoutEnding };
+        }
+        return svgDefinition;
+      }
+    )
+  );
+
+  // This will filter out null values
+  return svgDefinitions.filter(svgDefinition => svgDefinition);
+};
